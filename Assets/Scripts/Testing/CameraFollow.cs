8c857b3b03381b3e84fc55b0f5f672a759cbfa7d
RayYe <turtle50vp@gmail.com>
2018-04-18 12:54:40 -0400

    Assembling game field, finished splitscreen

--- Assets/Scripts/Testing/CameraFollow.cs
+++ Assets/Scripts/Testing/CameraFollow.cs
@@ -6,15 +6,23 @@ public class CameraFollow : MonoBehaviour {
 
 	// Use this for initialization
 	public float maxrad = 4.0f;
+	public GameObject player;
+	public GameManager gm;
+	bool isPlayer1;
 	void Start () {
-
+		string thisobj = this.name;
+		string plnum = thisobj.Substring(thisobj.Length - 1);
+		if(plnum == "1"){
+			isPlayer1 = true;
+		}
+		else{
+			isPlayer1 = false;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		GameManager gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+	void InPlayCamera(){
 		Vector2 thispos = this.transform.position;
-		Vector2 playerpos = gm.player.transform.position;
+		Vector2 playerpos = player.transform.position;
 		Vector2 change;
 		float dist = 0f;
 		bool isOutsideRad;
@@ -22,6 +30,13 @@ public class CameraFollow : MonoBehaviour {
 		dist = thispos.magnitude;
 		isOutsideRad = dist > maxrad;
 		if(isOutsideRad){
+			if(dist > 70f){
+				this.transform.position = new Vector3(
+											playerpos.x,
+											playerpos.y,
+											this.transform.position.z);
+				return;
+			}
 			change = thispos.normalized*(dist - maxrad);
 		}
 		else{
@@ -29,7 +44,45 @@ public class CameraFollow : MonoBehaviour {
 		}
 		thispos = this.transform.position;
 		this.transform.position = new Vector3(thispos.x + change.x,
-											  thispos.y + change.y,
-											  this.transform.position.z);
+											thispos.y + change.y,
+											this.transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gm.startingMenu){
+			if(isPlayer1){
+				this.transform.position = new Vector3(gm.p1startloc.x,
+													gm.p1startloc.y,
+													this.transform.position.z);
+			}
+			else{
+				this.transform.position = new Vector3(gm.p2startloc.x,
+													gm.p2startloc.y,
+													this.transform.position.z);
+			}
+		}
+		else{
+			if(isPlayer1){
+				if(gm.isViewingMinimapP1){
+					this.transform.position = new Vector3(gm.viewloc.x,
+													gm.viewloc.y,
+													this.transform.position.z);
+				}
+				else{
+					InPlayCamera();
+				}
+			}
+			else{
+				if(gm.isViewingMinimapP2){
+					this.transform.position = new Vector3(gm.viewloc.x,
+													gm.viewloc.y,
+													this.transform.position.z);
+				}
+				else{
+					InPlayCamera();
+				}
+			}
+		}
 	}
 }
